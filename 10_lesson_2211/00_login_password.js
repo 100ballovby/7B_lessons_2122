f4767d3568993cca6b24870091cee331d9795d4b
demidraksin <greatraksin@icloud.com>
2021-11-22 14:00:29 +0300

    Adding conditions

---
+++ 10_lesson_2211/00_login_password.js
@@ -0,0 +1,14 @@
+let login = 'zodiac';
+let password = '123456qwerty';
+
+if (login === 'zodiac') {
+    if (password === 'fright') {
+        console.log('Добро пожаловать')
+    }
+    else {
+        console.log('уопрщ3шкурпщ34')
+    }
+} else {
+    console.log('уопрщ3шкурпщ34')
+}
+
